26b2b5d83a61926b0cbaf6a86694c2e09e5e5646
avi12 <avi6106@gmail.com>
2021-07-27 11:28:42 +0300

    Background script: Keeping track of newly-opening tabs

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,8 +1,20 @@
-import { setStorage } from "./utils";
+import { getVideoId, setStorage } from "./utils";
 import { createFFmpeg } from "@ffmpeg/ffmpeg";
 
+type MediaType = "video" | "playlist" | "other";
+
 let gFfmpeg;
-const gVideos = [];
+const gTracker = {
+  videoQueue: [] as string[],
+  tabs: new Map() as Map<
+    number,
+    {
+      type: MediaType;
+      id: string;
+      idVideos?: string[];
+    }
+  >
+};
 
 async function initializeFFmpeg() {
   await setStorage("local", "isFFmpegReady", false);
@@ -13,12 +25,24 @@ async function initializeFFmpeg() {
   await setStorage("local", "isFFmpegReady", true);
 }
 
+function getMediaType(url: string): MediaType {
+  const urlObj = new URL(url);
+  if (urlObj.pathname === "/watch") {
+    return "video";
+  }
+  if (urlObj.pathname === "/playlist") {
+    return "playlist";
+  }
+
+  return "other";
+}
+
 function listenToTabs() {
   chrome.runtime.onConnect.addListener(port => {
     if (port.name === "youtube-page") {
-      port.onMessage.addListener(message => {
-        if (message) {
-        }
+      gTracker.tabs.set(port.sender.tab.id, {
+        type: getMediaType(port.sender.url),
+        id: getVideoId(port.sender.tab.url)
       });
     }
     port.onDisconnect.addListener(() => {});
@@ -26,8 +50,8 @@ function listenToTabs() {
 }
 
 async function init() {
-  await initializeFFmpeg();
   listenToTabs();
+  await initializeFFmpeg();
 }
 
 init();
