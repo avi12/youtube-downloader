d038c6c26f82c6b778c9ae52fa3accece9b324e3
avi12 <avi6106@gmail.com>
2021-07-27 16:01:59 +0300

    Content script initialize: Better handling FFmpeg's availability; retrieving video metadata

--- src/scripts/yt-downloader-content-script-initialize.ts
+++ src/scripts/yt-downloader-content-script-initialize.ts
@@ -1,6 +1,13 @@
 import $ from "jquery";
 import Port = chrome.runtime.Port;
 import { getStorage } from "./utils";
+import type { VideoData } from "./types";
+
+declare global {
+  interface Window {
+    videoDataRaw: VideoData;
+  }
+}
 
 let gPort: Port;
 
@@ -8,9 +15,29 @@ function attachToBackground() {
   gPort = chrome.runtime.connect({ name: "youtube-page" });
 }
 
+async function waitForFFmpeg() {
+  return new Promise(async resolve => {
+    const isFFmpegReady = (await getStorage("local", "isFFmpegReady")) ?? false;
+
+    if (isFFmpegReady) {
+      resolve(true);
+      return;
+    }
+
+    const listenToFFmpeg = changes => {
+      if (changes?.isFFmpegReady.newValue) {
+        resolve(true);
+        chrome.storage.onChanged.removeListener(listenToFFmpeg);
+      }
+    };
+
+    chrome.storage.onChanged.addListener(listenToFFmpeg);
+  });
+}
+
 async function handleFFmpegReadiness() {
   const $body = $("body");
-  const isFFmpegReady = (await getStorage("local", "isFFmepgReady")) ?? false;
+  const isFFmpegReady = (await getStorage("local", "isFFmpegReady")) ?? false;
   $body.data("ffmpeg-ready", isFFmpegReady as boolean);
 
   chrome.storage.onChanged.addListener(changes => {
@@ -22,9 +49,18 @@ async function handleFFmpegReadiness() {
   });
 }
 
-async function init() {
-  attachToBackground();
-  await handleFFmpegReadiness();
+async function getVideoMetadata(): Promise<VideoData> {
+  return new Promise(resolve => {
+    gPort.postMessage("get-metadata");
+    gPort.onMessage.addListener(resolve);
+  });
 }
 
+async function init() {
+  attachToBackground();
+  await waitForFFmpeg();
+  await handleFFmpegReadiness();
+
+  window.videoDataRaw = await getVideoMetadata();
+}
 init();
