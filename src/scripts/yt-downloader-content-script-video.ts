48d0e3b919a36768b25ea28bfa53efe516214483
avi12 <avi6106@gmail.com>
2021-08-12 07:33:27 +0300

    Content script video: added a download button & a basic interactivity

--- src/scripts/yt-downloader-content-script-video.ts
+++ src/scripts/yt-downloader-content-script-video.ts
@@ -1,6 +1,11 @@
 import { getVideoData } from "./yt-downloader-functions";
-import { gCancelControllers } from "./yt-downloader-content-script-initialize";
+import {
+  gCancelControllers,
+  gPorts
+} from "./yt-downloader-content-script-initialize";
 import type { PlayerResponse } from "./types";
+import Vue from "vue/dist/vue.js";
+import { getElementEventually } from "./utils";
 
 export async function handleVideo(): Promise<void> {
   const getHtml = async () => {
@@ -12,17 +17,113 @@ export async function handleVideo(): Promise<void> {
     return response.text();
   };
 
-  window.videoDataRaw = await getVideoData(await getHtml());
-
-  document.querySelector("video").dataset.ytDownloaderVideoData =
-    JSON.stringify(window.videoDataRaw);
+  window.videoData = await getVideoData(await getHtml());
 
   const getIsLive = (videoDataRaw: PlayerResponse) =>
     videoDataRaw.microformat?.playerMicroformatRenderer.liveBroadcastDetails
       ?.isLiveNow;
 
-  if (getIsLive(window.videoDataRaw)) {
-    // TODO: Add "Undownloadable"
-    return;
-  }
+  const elDownloaderContainer = document.createElement("div");
+  elDownloaderContainer.id = "ytdl-download-container";
+
+  const elButtonAfterRating = await getElementEventually(
+    "#top-level-buttons-computed ytd-button-renderer"
+  );
+  elButtonAfterRating.parentElement.insertBefore(
+    elDownloaderContainer,
+    elButtonAfterRating
+  );
+
+  const downloadContainer = new Vue({
+    el: "#ytdl-download-container",
+    data: {
+      isStartedDownload: false,
+      isDownloadable: !getIsLive(window.videoData),
+      progress: 0,
+      isQueued: false
+    },
+    template: `
+      <section>
+      <button @click="toggleDownload" :disabled="!isDownloadable">{{ textButton }}</button>
+      <progress :value="progress"></progress>
+      </section>
+    `,
+    computed: {
+      textButton() {
+        if (!this.isDownloadable) {
+          return "NOT DOWNLOADABLE";
+        }
+        if (!this.isStartedDownload) {
+          return "DOWNLOAD";
+        }
+        if (this.isInQueue) {
+          return "QUEUED";
+        }
+        return "CANCEL";
+      }
+    },
+    methods: {
+      toggleDownload() {
+        this.isStartedDownload = !this.isStartedDownload;
+
+        if (!this.isStartedDownload) {
+          gPorts.main.postMessage({ action: "cancel-download" });
+          return;
+        }
+
+        const { videoHeight, videoWidth } = document.querySelector("video");
+        const videoQuality = Math.min(videoHeight, videoWidth);
+
+        const {
+          videoDetails,
+          streamingData: { adaptiveFormats }
+        } = window.videoData;
+
+        const formatsSorted = adaptiveFormats.sort(
+          (a, b) => b.bitrate - a.bitrate
+        );
+        const video = formatsSorted.find(
+          format =>
+            format.mimeType.startsWith("video") &&
+            format.height === videoQuality
+        );
+
+        const audioBest = formatsSorted.find(format =>
+          format.mimeType.startsWith("audio")
+        );
+
+        gPorts.processMedia.postMessage({
+          type: "video+audio",
+          urls: {
+            video: video.url,
+            audio: audioBest.url
+          },
+          filenameOutput: `${videoDetails.title}.mp4`,
+          videoId: videoDetails.videoId
+        });
+      }
+    }
+  });
+
+  gPorts.processMedia.onMessage.addListener(({ progress }) => {
+    downloadContainer.progress = progress;
+    if (progress === 100) {
+      downloadContainer.isStartedDownload = false;
+    }
+  });
+
+  chrome.storage.onChanged.addListener(({ tracker }) => {
+    if (!tracker?.newValue.videoQueue) {
+      return;
+    }
+
+    const { videoId } = window.videoData.videoDetails;
+    const isQueued = tracker.newValue.videoQueue.includes(videoId);
+    const isCurrentlyInDownload = tracker.newValue.videoQueue[0] === videoId;
+    if (!isQueued) {
+      return;
+    }
+    downloadContainer.isQueued = isQueued && !isCurrentlyInDownload;
+    downloadContainer.isStartedDownload = isCurrentlyInDownload;
+  });
 }
