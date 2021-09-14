d5b3f02e03f8dc6af6e493ea05e4d370b0307226
avi12 <avi6106@gmail.com>
2021-09-14 08:53:43 +0300

    Utils: Added functions to get elements; added support for custom file extensions

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,16 +1,16 @@
-import type { MusicQueue, VideoQueue } from "./types";
+import type { MusicQueue, VideoOnlyQueue, VideoQueue } from "./types";
 
-export async function updateVideoQueue(videoQueue: VideoQueue): Promise<void> {
-  await setLocalStorage("videoQueue", videoQueue);
-}
-
-export async function updateMusicQueue(musicQueue: MusicQueue): Promise<void> {
-  await setLocalStorage("musicQueue", musicQueue);
+export async function updateQueue(
+  queueType: "video" | "videoOnly" | "music",
+  queue: VideoQueue | MusicQueue | VideoOnlyQueue
+): Promise<void> {
+  await setLocalStorage(`${queueType}Queue`, queue);
 }
 
 export async function setLocalStorage(
   key:
     | "videoQueue"
+    | "videoOnlyQueue"
     | "musicQueue"
     | "tabTracker"
     | "videoDetails"
@@ -27,6 +27,7 @@ export async function setLocalStorage(
 export async function getLocalStorage(
   key:
     | "videoQueue"
+    | "videoOnlyQueue"
     | "musicQueue"
     | "tabTracker"
     | "videoDetails"
@@ -55,10 +56,26 @@ async function getElementByObserver(selector: string): Promise<HTMLElement> {
   });
 }
 
+async function getElementsByObserver(selector: string): Promise<HTMLElement[]> {
+  return new Promise(resolve => {
+    const observerHtml = new MutationObserver((_, observer) => {
+      const elements = getVisibleElementsInArray(selector);
+      if (elements) {
+        observer.disconnect();
+        resolve(elements as HTMLElement[]);
+      }
+    });
+    observerHtml.observe(document.documentElement, {
+      childList: true,
+      subtree: true
+    });
+  });
+}
+
 export function isElementVisible(element: HTMLElement | Element): boolean {
   return (
-    (<HTMLElement>element).offsetWidth > 0 &&
-    (<HTMLElement>element).offsetHeight > 0
+    (<HTMLElement>element)?.offsetWidth > 0 &&
+    (<HTMLElement>element)?.offsetHeight > 0
   );
 }
 
@@ -71,6 +88,15 @@ function getVisibleElementInArray(
   return [...(elements as NodeListOf<HTMLElement>)].find(isElementVisible);
 }
 
+function getVisibleElementsInArray(
+  elements: NodeListOf<Element> | string
+): HTMLElement[] {
+  if (typeof elements === "string") {
+    elements = document.querySelectorAll(elements);
+  }
+  return [...(elements as NodeListOf<HTMLElement>)].filter(isElementVisible);
+}
+
 export async function getElementEventually(selector: string): Promise<Element> {
   const elements = document.querySelectorAll(selector);
   return (
@@ -79,34 +105,82 @@ export async function getElementEventually(selector: string): Promise<Element> {
   );
 }
 
+export async function getElementsEventually(
+  selector: string
+): Promise<Element[]> {
+  const elements = document.querySelectorAll(selector);
+  return (
+    (elements.length > 0 && getVisibleElementsInArray(elements)) ||
+    (await getElementsByObserver(selector))
+  );
+}
+
 export function getVideoId(url: string): string | null {
   const urlParams = new URLSearchParams(new URL(url).search);
   return urlParams.get("v");
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function hasOwnProperty(object: object, key: string): boolean {
-  return Object.prototype.hasOwnProperty.call(object, key);
+export function isElementInViewport(el: Element): boolean {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 && rect.bottom + 50 <= document.documentElement.clientHeight
+  );
 }
 
-export const gExtToMime = {
+export const gExtToMimeAll = {
+  au: "audio/basic",
+  snd: "audio/basic",
+  aif: "audio/x-aiff",
+  aifc: "audio/x-aiff",
+  aiff: "audio/x-aiff",
+  m3u: "audio/x-mpegurl",
+  ra: "audio/vnd.rn-realaudio",
+  ram: "audio/vnd.rn-realaudio",
   aac: "audio/aac",
   avi: "video/x-msvideo",
+  wmv: "video/x-ms-wmv",
   mp3: "audio/mpeg",
+  m4a: "audio/mp4",
   mp4: "video/mp4",
   mpeg: "video/mpeg",
-  oga: "audio/ogg",
+  ogg: "audio/ogg",
   ogv: "video/ogg",
+  oga: "audio/ogg",
+  spx: "audio/ogg",
+  ogm: "audio/ogg",
   opus: "audio/opus",
   ts: "video/mp2t",
-  wav: "audio/wav",
+  wav: "audio/vnd.wav",
   weba: "audio/webm",
   webm: "video/webm",
   "3gp": "video/3gpp",
   "3g2": "video/3gpp2"
 };
 
+export const gExtToMime = {
+  video: Object.fromEntries(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    Object.entries(gExtToMimeAll).filter(([_, mimeType]) =>
+      mimeType.startsWith("video")
+    )
+  ),
+  audio: Object.fromEntries(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    Object.entries(gExtToMimeAll).filter(([_, mimeType]) =>
+      mimeType.startsWith("audio")
+    )
+  )
+};
+
+export const gSupportedExts = {
+  video: Object.entries(gExtToMime.video).map(([ext]) => ext),
+  audio: Object.entries(gExtToMime.audio).map(([ext]) => ext)
+};
+
+export function getFileExt(filename: string): string {
+  return filename.split(".").pop();
+}
+
 export function getMimeType(filename: string): string | undefined {
-  const ext = filename.split(".").pop();
-  return gExtToMime[ext];
+  return gExtToMimeAll[getFileExt(filename)];
 }
