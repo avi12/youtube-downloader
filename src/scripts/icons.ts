85b929352569b0a0a67fe3445a96f1edbbf8e019
avi12 <avi6106@gmail.com>
2021-09-14 21:35:06 +0300

    Added SVG-based icons file

---
+++ src/scripts/icons.ts
@@ -0,0 +1,54 @@
+export const icons = {
+  download: `
+<svg xmlns="http://www.w3.org/2000/svg"  height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+  <g>
+    <rect fill="none" height="24" width="24" />
+  </g>
+  <g>
+    <path d="M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M17,11l-1.41-1.41L13,12.17V4h-2v8.17L8.41,9.59L7,11l5,5 L17,11z" />
+  </g>
+</svg>`,
+  cancelDownload: `
+<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+  <path d="M0 0h24v24H0V0z" fill="none" />
+  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+</svg>`,
+  notDownloadable: `
+<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+  <rect fill="none" height="24" width="24" />
+  <path d="M18,15.17V15h2v2.17L18,15.17z M15.41,12.59L17,11l-1.41-1.41L14,11.17L15.41,12.59z M13,10.17V4h-2v4.17L13,10.17z M21.19,21.19l-1.78-1.78L2.81,2.81L1.39,4.22l6.19,6.19L7,11l5,5l0.59-0.59L15.17,18H6v-3H4v3c0,1.1,0.9,2,2,2h11.17l2.61,2.61 L21.19,21.19z" />
+</svg>`,
+  downloadCompleted: `
+<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+  <g>
+    <rect fill="none" height="24" width="24" />
+  </g>
+  <g>
+    <polygon points="20.13,5.41 18.72,4 9.53,13.19 5.28,8.95 3.87,10.36 9.53,16.02" />
+    <rect height="2" width="14" x="5" y="18" />
+  </g>
+</svg>`,
+  downloadQueue: `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">    
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M10.2547 20H6C4.9 20 4 19.1 4 18V15H6V18H9.28988C9.50489 18.7224 9.83338 19.396 10.2547 20ZM13 9.67363V4H11V11.101C11.5741 10.5151 12.2504 10.0297 13 9.67363ZM10.4989 11.6708L8.41 9.59L7 11L9.47085 13.4709C9.72403 12.8177 10.0724 12.212 10.4989 11.6708Z" />    
+    <path d="M15.994 10C12.682 10 10 12.688 10 16C10 19.312 12.682 22 15.994 22C19.312 22 22 19.312 22 16C22 12.688 19.312 10 15.994 10ZM16 20.8C13.348 20.8 11.2 18.652 11.2 16C11.2 13.348 13.348 11.2 16 11.2C18.652 11.2 20.8 13.348 20.8 16C20.8 18.652 18.652 20.8 16 20.8ZM16.3 13H15.4V16.6L18.55 18.49L19 17.752L16.3 16.15V13Z" />
+  </svg>
+`,
+  expand: `
+  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
+  </svg>
+`,
+  toggleOn: `
+<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+  <path d="M0 0h24v24H0z" fill="none" />
+  <path d="M17 7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h10c2.76 0 5-2.24 5-5s-2.24-5-5-5zm0 8c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z" />
+</svg>
+`,
+  toggleOff: `
+<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+  <path d="M0 0h24v24H0z" fill="none" />
+  <path d="M17 7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h10c2.76 0 5-2.24 5-5s-2.24-5-5-5zM7 15c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z" />
+</svg>`
+};
