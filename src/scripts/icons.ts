b97abb215d08e657609d724fcbc558f705af39f8
avi12 <avi6106@gmail.com>
2021-09-29 13:37:35 +0300

    Icons: Simplified SVGs; added IDs so they can be used by using `<use>`

--- src/scripts/icons.ts
+++ src/scripts/icons.ts
@@ -1,64 +1,63 @@
 export const icons = {
   download: `
-<svg viewBox="0 0 24 24" width="24" height="24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon"> 
-  <g class="style-scope yt-icon">
-    <path d="M17 18V19H6V18H17ZM16.5 11.4L15.8 10.7L12 14.4V4H11V14.4L7.2 10.6L6.5 11.3L11.5 16.3L16.5 11.4Z" class="style-scope yt-icon" />
-  </g>
-</svg>`,
+<svg id="ytdl-svg__download" width="24" height="24"> 
+    <path d="M17 18V19H6V18H17ZM16.5 11.4L15.8 10.7L12 14.4V4H11V14.4L7.2 10.6L6.5 11.3L11.5 16.3L16.5 11.4Z"></path>
+</svg>
+`,
   downloadAll: `
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M20.18 18.67V18H4.18V18.67H20.18Z" />
-  <path d="M20.3636 10.7333L19.9182 10.2667L17.5 12.7333V6H16.8636V12.7333L14.4455 10.2L14 10.6667L17.1818 14L20.3636 10.7333Z" />
-  <path d="M15.3618 13.7333L14.9164 13.2667L12.4982 15.7333V6H11.8618V15.7333L9.44364 13.2L8.99818 13.6667L12.18 17L15.3618 13.7333Z" />
-  <path d="M10.3636 10.7333L9.91818 10.2667L7.5 12.7333V6H6.86364V12.7333L4.44545 10.2L4 10.6667L7.18182 14L10.3636 10.7333Z" />
+<svg id="ytdl-svg__download-all" fill="none" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+  <path d="M20.18 18.67V18H4.18V18.67H20.18Z"></path>
+  <path d="M20.3636 10.7333L19.9182 10.2667L17.5 12.7333V6H16.8636V12.7333L14.4455 10.2L14 10.6667L17.1818 14L20.3636 10.7333Z"></path>
+  <path d="M15.3618 13.7333L14.9164 13.2667L12.4982 15.7333V6H11.8618V15.7333L9.44364 13.2L8.99818 13.6667L12.18 17L15.3618 13.7333Z"></path>
+  <path d="M10.3636 10.7333L9.91818 10.2667L7.5 12.7333V6H6.86364V12.7333L4.44545 10.2L4 10.6667L7.18182 14L10.3636 10.7333Z"></path>
 </svg>`,
   cancelDownload: `
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-  <path d="M0 0h24v24H0V0z" fill="none" />
-  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+<svg id="ytdl-svg__cancel-download" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0 0h24v24H0V0z" fill="none"></path>
+  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"></path>
 </svg>`,
   notDownloadable: `
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <rect x="4.70235" y="4.57395" width="21.6075" height="0.993271" transform="rotate(45 4.70235 4.57395)" />
-  <path fill-rule="evenodd" clip-rule="evenodd" d="M17.3613 17.2624L18.3801 18.2811H7.80466V17.2624H17.3613ZM15.0681 13.5644L18.0152 10.5387L17.3345 9.82555L14.3617 12.858L15.0681 13.5644ZM13.6663 13.5673L14.375 14.276L13.153 15.5305L8.29087 10.4368L8.97158 9.72368L12.6668 13.5949V12.5678L13.6393 13.5403V13.5949L13.6663 13.5673ZM13.6393 12.1356V3H12.6668V11.1631L13.6393 12.1356Z" />
+<svg id="ytdl-svg__not-downloadable" fill="none" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+  <rect height="0.993271" transform="rotate(45 4.70235 4.57395)" width="21.6075" x="4.70235" y="4.57395"></rect>
+  <path clip-rule="evenodd" d="M17.3613 17.2624L18.3801 18.2811H7.80466V17.2624H17.3613ZM15.0681 13.5644L18.0152 10.5387L17.3345 9.82555L14.3617 12.858L15.0681 13.5644ZM13.6663 13.5673L14.375 14.276L13.153 15.5305L8.29087 10.4368L8.97158 9.72368L12.6668 13.5949V12.5678L13.6393 13.5403V13.5949L13.6663 13.5673ZM13.6393 12.1356V3H12.6668V11.1631L13.6393 12.1356Z" fill-rule="evenodd"></path>
 </svg>`,
   downloadCompleted: `
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+<svg id="ytdl-svg__download-completed" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
   <g>
-    <rect fill="none" height="24" width="24" />
+    <rect fill="none" height="24" width="24"></rect>
   </g>
   <g>
-    <polygon points="20.13,5.41 18.72,4 9.53,13.19 5.28,8.95 3.87,10.36 9.53,16.02" />
-    <rect height="2" width="14" x="5" y="18" />
+    <polygon points="20.13,5.41 18.72,4 9.53,13.19 5.28,8.95 3.87,10.36 9.53,16.02"></polygon>
+    <rect height="2" width="14" x="5" y="18"></rect>
   </g>
 </svg>`,
   downloadQueue: `
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" clip-rule="evenodd" d="M8.83222 18C8.99809 18.3503 9.19214 18.6846 9.41144 19H6V18H8.83222ZM12 8.75194V4H11V9.37068C11.3142 9.13774 11.6486 8.93046 12 8.75194ZM8.76177 12.1618C8.61421 12.4939 8.49172 12.8397 8.3967 13.1967L6.5 11.3L7.2 10.6L8.76177 12.1618Z" />
-  <path d="M14.8396 8.70261C11.4424 8.70261 8.69145 11.4597 8.69145 14.8569C8.69145 18.254 11.4424 21.0112 14.8396 21.0112C18.2429 21.0112 21 18.254 21 14.8569C21 11.4597 18.2429 8.70261 14.8396 8.70261ZM14.8457 19.7803C12.1255 19.7803 9.9223 17.5771 9.9223 14.8569C9.9223 12.1367 12.1255 9.93346 14.8457 9.93346C17.5659 9.93346 19.7691 12.1367 19.7691 14.8569C19.7691 17.5771 17.5659 19.7803 14.8457 19.7803ZM15.1534 11.7797H14.2303V15.4723L17.4613 17.4109L17.9229 16.6539L15.1534 15.0107V11.7797Z" />
+<svg id="ytdl-svg__download-queue" fill="none" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+  <path clip-rule="evenodd" d="M8.83222 18C8.99809 18.3503 9.19214 18.6846 9.41144 19H6V18H8.83222ZM12 8.75194V4H11V9.37068C11.3142 9.13774 11.6486 8.93046 12 8.75194ZM8.76177 12.1618C8.61421 12.4939 8.49172 12.8397 8.3967 13.1967L6.5 11.3L7.2 10.6L8.76177 12.1618Z" fill-rule="evenodd"></path>
+  <path d="M14.8396 8.70261C11.4424 8.70261 8.69145 11.4597 8.69145 14.8569C8.69145 18.254 11.4424 21.0112 14.8396 21.0112C18.2429 21.0112 21 18.254 21 14.8569C21 11.4597 18.2429 8.70261 14.8396 8.70261ZM14.8457 19.7803C12.1255 19.7803 9.9223 17.5771 9.9223 14.8569C9.9223 12.1367 12.1255 9.93346 14.8457 9.93346C17.5659 9.93346 19.7691 12.1367 19.7691 14.8569C19.7691 17.5771 17.5659 19.7803 14.8457 19.7803ZM15.1534 11.7797H14.2303V15.4723L17.4613 17.4109L17.9229 16.6539L15.1534 15.0107V11.7797Z"></path>
 </svg>
 `,
   expand: `
-  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-  </svg>
+<svg id="ytdl-svg__expand" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0 0h24v24H0z" fill="none"></path>
+  <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+</svg>
 `,
   toggleOn: `
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path d="M17 7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h10c2.76 0 5-2.24 5-5s-2.24-5-5-5zm0 8c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z" />
+<svg id="ytdl-svg__toggle-on" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0 0h24v24H0z" fill="none"></path>
+  <path d="M17 7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h10c2.76 0 5-2.24 5-5s-2.24-5-5-5zm0 8c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"></path>
 </svg>
 `,
   toggleOff: `
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path d="M17 7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h10c2.76 0 5-2.24 5-5s-2.24-5-5-5zM7 15c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z" />
+<svg id="ytdl-svg__toggle-off" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0 0h24v24H0z" fill="none"></path>
+  <path d="M17 7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h10c2.76 0 5-2.24 5-5s-2.24-5-5-5zM7 15c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"></path>
 </svg>`,
   options: `
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"> 
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z" />
+<svg id="ytdl-svg__options" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0 0h24v24H0z" fill="none"></path>
+  <path d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z"></path>
 </svg>
 `
 };
